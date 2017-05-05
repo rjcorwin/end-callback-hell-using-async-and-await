d267ae6f55267e3fad382e18b09fc575698ddb00
R.J. Steinert <rj@rjsteinert.com>
2017-05-05 11:54:01 -0400

    Big updates for examples

--- basic-example/README.md
+++ basic-example/README.md
@@ -1,16 +1,7 @@
+# Basic HTTP Request Example
+
+Build and run this example using Docker Compose to ensure you are running this with the correct version of Node.js.
 ```
 docker-compose up --build
 ```
 
-Waiting for web services to be ready...
-
-```
-app_1  | 
-app_1  | Waiting until datbase is ready...
-app_1  | Database not ready. Sleeping one second...
-db_1   | Apache CouchDB 1.6.1 (LogLevel=info) is starting.
-db_1   | Apache CouchDB has started. Time to relax.
-db_1   | [info] [<0.32.0>] Apache CouchDB has started on http://0.0.0.0:5984/
-db_1   | [info] [<0.104.0>] 172.30.0.2 - - GET / 200
-app_1  | Database is ready!
-```
--- build-async-enabled-functions/index.js
+++ build-async-enabled-functions/index.js
@@ -1,16 +1,35 @@
 // Use axios for an http library. Axios returns a Promise which makes it await-able. 
 var http = require('axios')
 
+// A function you can `await` is a function that returns a Promise. Inside that Promise you do something asynchronous 
+// and then call that Promise's `resolve` function which is then what signals to await to continue. 
+const sleep = function (delay) {  
+  return new Promise(function (resolve, reject) {
+    setTimeout(() => {
+      resolve()
+    }, delay)
+  })
+}
+
 // Our app function. Note how our use of await is encapsulated by a function that uses async.
 async function app () {  
  
+  console.log('Waiting 3 seconds...')
+  await sleep(3000)
+
   console.log('First request.')
   var response = await http.get('http://api.github.com/repos/nodejs/node')
   console.log(response.data)
 
+  console.log('Waiting 3 seconds...')
+  await sleep(3000)
+
   console.log('Second request.')
   var response = await http.get('http://api.github.com/repos/nodejs/http2')
   console.log(response.data)
+
+  console.log('Waiting 3 seconds...')
+  await sleep(3000)
 
   console.log('Third request.')
   var response = await http.get('http://api.github.com/repos/nodejs/docker-node')
